567eea7c04e273ed8ff7af3eecfb289026272215
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2019-11-24 12:36:19 +0000

    Create a bugcontroller with AddBug method

---
+++ WebAPI/WebAPI/Controllers/BugController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    public class BugController : ControllerBase
+    {
+        private readonly AuthenticationContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public BugController(AuthenticationContext db, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+
+        [HttpPost("api/bug")]
+        public async Task<IActionResult> AddBug()
+        {
+            string userId = User.Claims.First(c => c.Type == "UserID").Value;
+
+            var user = await _userManager.FindByIdAsync(userId);
+
+            _db.Bugs.Add(new Bug
+            {
+                ApplicationUserId = userId,
+                Description = $"{user.FullName} bug description",
+                Title = $"{user.FullName} bug title"
+            });
+
+            await _db.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
