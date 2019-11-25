bc85073aa14918de4db7b7af669f16606782ac69
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2019-11-25 17:06:21 +0000

    added bug model; modified home.component.html

--- Angular/src/app/bug/bug-entry/bug-entry.component.ts
+++ Angular/src/app/bug/bug-entry/bug-entry.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-bug-entry',
+  selector: 'bug-entry',
   templateUrl: './bug-entry.component.html',
   styles: []
 })
